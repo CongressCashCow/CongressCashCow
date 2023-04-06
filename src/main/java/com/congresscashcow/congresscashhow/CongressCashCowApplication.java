56df2174f9b81efc07fdd2e33d608adcc0a19656
Jona Gonzalez <jona.a.gonzalez104@gmail.com>
2023-04-06 10:37:43 -0500

    added spring init

---
+++ src/main/java/com/congresscashcow/congresscashhow/CongressCashCowApplication.java
@@ -0,0 +1,14 @@
+package com.congresscashcow.congresscashhow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CongressCashCowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CongressCashCowApplication.class, args);
+
+    }
+
+}
---
+++ src/test/java/com/congresscashcow/congresscashhow/CongressCashCowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.congresscashcow.congresscashhow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CongressCashCowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
