c02dcb409d207df216922557177b0c340a6fd7fc
Jeffrey Neal <jeffneal11@gmail.com>
2023-04-14 16:50:43 -0500

    added api endpoints to sec bypass

--- src/main/java/com/milkmoney/config/SecurityConfiguration.java
+++ src/main/java/com/milkmoney/config/SecurityConfiguration.java
@@ -46,7 +46,7 @@ public class SecurityConfiguration {
                 .and()
                 .authorizeHttpRequests()
                 .requestMatchers(
-                        "/politician-profile", "/user-profile", "/index-user", "api"
+                        "/politician-profile", "/user-profile", "/index-user", "/api", "/api/recent","/api/recent/censored"
                 )
                 .authenticated()
                 .and()
