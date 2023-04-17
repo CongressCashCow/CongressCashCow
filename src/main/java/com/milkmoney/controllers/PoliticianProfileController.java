489fdf29ace4771f1f0e210012fb8f72b9aa5b94
Fernando Rivera <fernando.rivera0011@gmail.com>
2023-04-17 11:41:32 -0500

    /politician-profile controller updated

--- src/main/java/com/milkmoney/controllers/PoliticianProfileController.java
+++ src/main/java/com/milkmoney/controllers/PoliticianProfileController.java
@@ -1,4 +1,12 @@
 package com.milkmoney.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
 public class PoliticianProfileController {
+    @GetMapping("/politician-profile")
+    public String politicianProfilePage() {
+
+        return "politician-profile";
+    }
 }
