4280331e330192993cf309966b8b546cb8a5b902
Jona Gonzalez <jona.a.gonzalez104@gmail.com>
2023-04-24 10:58:45 -0500

    added edits to the cards

--- src/main/java/com/milkmoney/controllers/HomeController.java
+++ src/main/java/com/milkmoney/controllers/HomeController.java
@@ -13,6 +13,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -127,6 +128,11 @@ public class HomeController {
         return "index-user";
 
     }
+
+//    @PostMapping("/index")
+//    public String returnUser(){
+//        return "redirect:/";
+//    }
 }
 
 
