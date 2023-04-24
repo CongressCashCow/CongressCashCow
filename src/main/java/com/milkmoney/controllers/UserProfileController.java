3b1b74b020835f575cc35a6fed2bdfb53f3d46b0
Jeffrey Neal <jeffneal11@gmail.com>
2023-04-24 09:13:30 -0500

    unfollow btn on user page

--- src/main/java/com/milkmoney/controllers/UserProfileController.java
+++ src/main/java/com/milkmoney/controllers/UserProfileController.java
@@ -1,5 +1,6 @@
 package com.milkmoney.controllers;
 
+import com.milkmoney.Repositories.PoliticianRepository;
 import com.milkmoney.Repositories.UserRepository;
 import com.milkmoney.models.Politician;
 import com.milkmoney.models.User;
@@ -7,6 +8,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -14,9 +17,12 @@ import java.util.List;
 public class UserProfileController {
 
     private final UserRepository userDAO;
+    private final PoliticianRepository politicianDAO;
 
-    public UserProfileController(UserRepository userDAO) {
+    public UserProfileController(UserRepository userDAO, PoliticianRepository politicianDAO) {
+
         this.userDAO = userDAO;
+        this.politicianDAO = politicianDAO;
     }
 
     @GetMapping("/user-profile")
@@ -30,5 +36,30 @@ public class UserProfileController {
 
         return "user-profile";
     }
+    @PostMapping("/user-profile")
+    public String rmFavorite(@RequestParam("pol_id") String name, @RequestParam("follow-btn") boolean follow) {
+
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        User fixedUser = userDAO.findById(currentUser.getId()).get();
+        boolean isPresent = false;
+        if(follow){
+            for(Politician p : fixedUser.getPoliticians()){
+                if(p.getName().equals(name)){
+                    isPresent = true;
+                    break;
+                }
+            }
+            if(!isPresent){
+                fixedUser.addPolitician(politicianDAO.findByName(name));
+                userDAO.save(fixedUser);
+            }
+        }else{
+            fixedUser.removePolitician(politicianDAO.findByName(name));
+            userDAO.save(fixedUser);
+        }
+
+        return "redirect:/user-profile";
+    }
+
 
 }
