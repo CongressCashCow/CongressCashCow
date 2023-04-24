d2dcaf0c0f6f2a8d4dfb1b63e54d6c9e4fd8c32e
Jeffrey Neal <jeffneal11@gmail.com>
2023-04-24 16:49:22 -0500

    index page beuausetification

--- src/main/java/com/milkmoney/controllers/HomeController.java
+++ src/main/java/com/milkmoney/controllers/HomeController.java
@@ -39,68 +39,14 @@ public class HomeController {
     }
 
 
-//    @GetMapping("/index-visitor")
-//    public String returnVisitor(Authentication authentication, Model model) {
-//        List<Trade> recentTrades = new ArrayList<>();
-//        List<Trade> trades = api.getTrades();
-//        List<String> tradeNames = new ArrayList<>();
-//        int f = 0;
-//
-////        if(SecurityContextHolder.getContext().getAuthentication().isAuthenticated()) {
-////            System.out.println(SecurityContextHolder.getContext().getAuthentication().getPrincipal().getClass());
-//        if (authentication != null && authentication.isAuthenticated()) {
-//            // User is authenticated, add user details to the model and return the name of the profile view
-////            User user = (User) authentication.getPrincipal();
-////            System.out.println("logged in");
-//
-//            User currentUser = (User) authentication.getPrincipal();
-//            User fixedUser = userDAO.findByUsername(currentUser.getUsername());
-//
-//            List<String> names = new ArrayList<>();
-//
-//            for (Politician p : fixedUser.getPoliticians()) {
-//                names.add(p.getName());
-//            }
-//            model.addAttribute("names",names);
-//        } else {
-//            System.out.println("logged out");
-//        }
-//
-//        for (Trade t : trades) {
-//            if (f < 20) {
-//                if (!tradeNames.contains(t.getPolitician().getName())) {
-//                    recentTrades.add(t);
-//                    tradeNames.add(t.getPolitician().getName());
-//                    f++;
-//                }
-//            } else {
-//                break;
-//            }
-//        }
-//
-//        model.addAttribute("trades",recentTrades);
-//        return "index-visitor";
-//    }
-
     @GetMapping("/index")
     public String returnUser(Authentication authentication, Model model) throws JsonProcessingException {
-        System.out.println("your inside the index user");
-//        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-        ObjectMapper mapper = new ObjectMapper();
-//        System.out.println(mapper.writeValueAsString(user));
-
         List<Trade> recentTrades = new ArrayList<>();
         List<Trade> trades = api.getTrades();
         List<String> tradeNames = new ArrayList<>();
         int f = 0;
 
-//        if(SecurityContextHolder.getContext().getAuthentication().isAuthenticated()) {
-//            System.out.println(SecurityContextHolder.getContext().getAuthentication().getPrincipal().getClass());
         if (authentication != null && authentication.isAuthenticated()) {
-            // User is authenticated, add user details to the model and return the name of the profile view
-//            User user = (User) authentication.getPrincipal();
-//            System.out.println("logged in");
-
             User currentUser = (User) authentication.getPrincipal();
             User fixedUser = userDAO.findByUsername(currentUser.getUsername());
 
@@ -115,7 +61,7 @@ public class HomeController {
         }
 
         for (Trade t : trades) {
-            if (f < 20) {
+            if (f <= 20) {
                 if (!tradeNames.contains(t.getPolitician().getName())) {
                     recentTrades.add(t);
                     tradeNames.add(t.getPolitician().getName());
