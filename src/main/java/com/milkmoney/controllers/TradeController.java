1c88f6691174976d7e4d2d37b8c739b76a20ab67
Fernando Rivera <fernando.rivera0011@gmail.com>
2023-04-25 15:22:18 -0500

    Fixed Merge Conflict

--- src/main/java/com/milkmoney/controllers/TradeController.java
+++ src/main/java/com/milkmoney/controllers/TradeController.java
@@ -1,5 +1,4 @@
 package com.milkmoney.controllers;
-
 import com.milkmoney.Repositories.PoliticianRepository;
 import com.milkmoney.Repositories.UserRepository;
 import com.milkmoney.Services.APIService;
@@ -30,63 +29,21 @@ public class TradeController {
         this.api = api;
     }
     @GetMapping("/trades")
-<<<<<<< HEAD
-    public String viewTrades(Model model, @RequestParam(required = false,value="searchbar") String searchQuery, @RequestParam(required = false,value="page",defaultValue = "1") String page,@RequestParam(required = false,value="arrow-btn") String pageDir){
-=======
     public String viewTrades(Authentication authentication, Model model, @RequestParam(required = false,value="searchbar") String searchQuery){
->>>>>>> main
         List<Trade> trades = api.getTrades();
-        List<Trade> middleman = new ArrayList<>();
-        String path = null;
-        int limit= 201;
-        if(page == null || page.equals("")){
-            page = "1";
-        }
-        int pageInt = Integer.parseInt(page);
-
-        if(pageDir != null){
-            if(pageDir.equals("right")){
-                pageInt++;
-            }else {
-                pageInt--;
-            }
-
-        }
+        List<Trade> searchResults = new ArrayList<>();
+        System.out.println(searchQuery);
         if(searchQuery != null && searchQuery.trim().length() > 0){
-            path = String.format("?searchbar=%s&page=%s",searchQuery,page);
             for(Trade t : trades){
                 if(t.getTicker().equalsIgnoreCase(searchQuery)){
-                    middleman.add(t);
+                    searchResults.add(t);
                 }
             }
-
-        }else {
-            path = String.format("?page=%s",page);
-            middleman = trades;
-        }
-        List<Trade> out = new ArrayList<>();
-
-        if(middleman.size() > limit){
-            int offset = (pageInt - 1) * limit;
-            for(Trade t : middleman){
-//                System.out.println(middleman.indexOf(t));
-                if(middleman.indexOf(t) > offset && middleman.indexOf(t) < offset + limit ){
-                    out.add(t);
-                }
-            }
+            model.addAttribute("trades", searchResults);
         }else{
-            for (Trade t : middleman){
-                out.add(t);
-            }
+            model.addAttribute("trades", trades);
         }
 
-<<<<<<< HEAD
-        model.addAttribute("trades", out);
-
-        model.addAttribute("path", path);
-        model.addAttribute("tradeTotal", middleman.size());
-        return "trades";
-=======
         if (authentication != null && authentication.isAuthenticated()) {
             System.out.println("logged in");
 
@@ -100,9 +57,8 @@ public class TradeController {
             }
             model.addAttribute("names", names);
         }
-            return "trades";
+        return "trades";
 
->>>>>>> main
     }
 
     @PostMapping("/trades")
