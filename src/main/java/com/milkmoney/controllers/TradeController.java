877069a3dd2e31d4982520c8a79a97baa33aad6e
Jeffrey Neal <jeffneal11@gmail.com>
2023-04-26 15:21:59 -0500

    paginated

--- src/main/java/com/milkmoney/controllers/TradeController.java
+++ src/main/java/com/milkmoney/controllers/TradeController.java
@@ -5,6 +5,7 @@ import com.milkmoney.Services.APIService;
 import com.milkmoney.models.Politician;
 import com.milkmoney.models.Trade;
 import com.milkmoney.models.User;
+import org.springframework.data.domain.*;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -28,24 +29,43 @@ public class TradeController {
         this.userDAO = userDAO;
         this.api = api;
     }
+
+    Page<Trade> toPage(List<Trade> list, int pagesize, int pageNo) {
+        int totalpages = list.size() / pagesize;
+        Pageable pageable = PageRequest.of(pageNo, pagesize, Sort.by("ticker"));
+        int max = pageNo >= totalpages? list.size():pagesize*(pageNo+1);
+        int min = pageNo > totalpages? max:pagesize*pageNo;
+        return new PageImpl<>(list.subList(min, max), pageable, list.size());
+    }
+
     @GetMapping("/trades")
-    public String viewTrades(Authentication authentication, Model model, @RequestParam(required = false,value="searchbar") String searchQuery){
+    public String viewTrades(Authentication authentication, Model model, @RequestParam(required = false,value="searchbar") String searchQuery, @RequestParam(required = false,value="page",defaultValue = "0") int pageNo){
         List<Trade> trades = api.getTrades();
         List<Trade> searchResults = new ArrayList<>();
-        System.out.println(searchQuery);
+
+        int pageLimit = 200;
+        if(pageNo < 0){
+            pageNo = 0;
+        }else if(pageNo > trades.size()/pageLimit){
+            pageNo = (int) Math.floor(trades.size()/pageLimit);
+        }
         if(searchQuery != null && searchQuery.trim().length() > 0){
             for(Trade t : trades){
                 if(t.getTicker().equalsIgnoreCase(searchQuery)){
                     searchResults.add(t);
                 }
             }
+            model.addAttribute("searched", true);
             model.addAttribute("trades", searchResults);
+            model.addAttribute("page", searchResults);
         }else{
+            Page<Trade> page = toPage(trades, pageLimit, pageNo);
+            model.addAttribute("searched", false);
             model.addAttribute("trades", trades);
+            model.addAttribute("page", page);
         }
 
         if (authentication != null && authentication.isAuthenticated()) {
-            System.out.println("logged in");
 
             User currentUser = (User) authentication.getPrincipal();
             User fixedUser = userDAO.findByUsername(currentUser.getUsername());
@@ -57,13 +77,15 @@ public class TradeController {
             }
             model.addAttribute("names", names);
         }
+        model.addAttribute("curPage",pageNo);
+
         return "trades";
 
     }
 
     @PostMapping("/trades")
-    public String addFavorite(@RequestParam("ticker") String ticker, @RequestParam("pol_id") String name,@RequestParam("follow-btn") boolean follow) {
-
+    public String addFavorite(@RequestParam("ticker") String ticker, @RequestParam("pol_id") String name,@RequestParam("follow-btn") boolean follow, @RequestParam("search") boolean didSearch, @RequestParam("thisPage") String thisPage) {
+//        System.out.printf("page value is %d%n",pageNo);
         User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         User fixedUser = userDAO.findById(currentUser.getId()).get();
         boolean isPresent = false;
@@ -83,10 +105,41 @@ public class TradeController {
             userDAO.save(fixedUser);
         }
 
-        String out=String.format("redirect:/trades?searchbar=%s", ticker);
+        String out=String.format("redirect:/trades?page=%s", thisPage);
+        if(didSearch){
+            out=String.format("redirect:/trades?searchbar=%s", ticker);
+        }
 
         return out;
 
     }
+
+//    @PostMapping("/trades/page")
+//    public String changePage(@RequestParam("ticker") String ticker, @RequestParam("pol_id") String name,@RequestParam("follow-btn") boolean follow, @RequestParam(required = false,value="page-change",defaultValue = "0") int pageNo) {
+//        System.out.printf("page value is %d%n",pageNo);
+//        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+//        User fixedUser = userDAO.findById(currentUser.getId()).get();
+//        boolean isPresent = false;
+//        if(follow){
+//            for(Politician p : fixedUser.getPoliticians()){
+//                if(p.getName().equals(name)){
+//                    isPresent = true;
+//                    break;
+//                }
+//            }
+//            if(!isPresent){
+//                fixedUser.addPolitician(politicianDAO.findByName(name));
+//                userDAO.save(fixedUser);
+//            }
+//        }else{
+//            fixedUser.removePolitician(politicianDAO.findByName(name));
+//            userDAO.save(fixedUser);
+//        }
+//
+//        String out=String.format("redirect:/trades?searchbar=%s&page=%n", ticker,pageNo);
+//
+//        return out;
+//
+//    }
 
 }
