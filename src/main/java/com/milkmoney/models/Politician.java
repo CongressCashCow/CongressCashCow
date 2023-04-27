45f6ffebcda686a72f2e439c53cfd73801babd80
Will Parry <wsparry99@gmail.com>
2023-04-27 14:28:41 -0500

    implemented trade total values on politician profile view

--- src/main/java/com/milkmoney/models/Politician.java
+++ src/main/java/com/milkmoney/models/Politician.java
@@ -1,5 +1,6 @@
 package com.milkmoney.models;
 
+import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -81,40 +82,73 @@ public class Politician {
         return trades.size();
     }
 
-    public long[] getTotals(){
-        long[] output = new long[4];
-        long stotalLow = 0;
-        long stotalHigh = 0;
-        long ptotalLow = 0;
-        long ptotalHigh = 0;
+//    public long[] getTotals(){
+//        long[] output = new long[4];
+//        long stotalLow = 0;
+//        long stotalHigh = 0;
+//        long ptotalLow = 0;
+//        long ptotalHigh = 0;
+//        for(Trade t : trades){
+//            System.out.println(t.getRange());
+//            if(t.getTransactionType().equals("Sale")) {
+//                if(t.getRange().contains("-")) {
+//                    String[] arr = t.getRange().replace(" ", "").replace(",", "").replace("$", "").split("-", 2);
+//                    stotalLow += Long.parseLong(arr[0]);
+//                    stotalHigh += Long.parseLong(arr[1]);
+//                }else{
+//                    stotalLow += Long.parseLong(t.getRange().replace(" ", "").replace(",", "").replace("$", "").replace("+", ""));
+//                    stotalHigh += Long.parseLong(t.getRange().replace(" ", "").replace(",", "").replace("$", "").replace("+", ""));
+//                }
+//            } else if (t.getTransactionType().equals("Purchase")) {
+//                if(t.getRange().contains("-")) {
+//                String[] arr = t.getRange().replace(" ", "").replace(",", "").replace("$", "").split("-", 2);
+//                ptotalLow += Long.parseLong(arr[0]);
+//                ptotalHigh += Long.parseLong(arr[1]);
+//                }else{
+//                    ptotalLow += Long.parseLong(t.getRange().replace(" ", "").replace(",", "").replace("$", "").replace("+", ""));
+//                    ptotalHigh += Long.parseLong(t.getRange().replace(" ", "").replace(",", "").replace("$", "").replace("+", ""));
+//
+//                }
+//            }
+//        }
+//        output[0] = stotalLow;
+//        output[1] = stotalHigh;
+//        output[2] = ptotalLow;
+//        output[3] = ptotalHigh;
+//
+//        return output;
+//    }
+
+
+    public BigDecimal[] getTotals(){
+        BigDecimal stotalLow = BigDecimal.ZERO;
+        BigDecimal stotalHigh = BigDecimal.ZERO;
+        BigDecimal ptotalLow = BigDecimal.ZERO;
+        BigDecimal ptotalHigh = BigDecimal.ZERO;
         for(Trade t : trades){
             if(t.getTransactionType().equals("Sale")) {
                 if(t.getRange().contains("-")) {
                     String[] arr = t.getRange().replace(" ", "").replace(",", "").replace("$", "").split("-", 2);
-                    stotalLow += Long.parseLong(arr[0]);
-                    stotalHigh += Long.parseLong(arr[1]);
-                }else{
-                    stotalLow += Long.parseLong(t.getRange().replace(" ", "").replace(",", "").replace("$", "").replace("+", ""));
-                    stotalHigh += Long.parseLong(t.getRange().replace(" ", "").replace(",", "").replace("$", "").replace("+", ""));
+                    stotalLow = stotalLow.add(new BigDecimal(arr[0]));
+                    stotalHigh = stotalHigh.add(new BigDecimal(arr[1]));
+                } else {
+                    stotalLow = stotalLow.add(new BigDecimal(t.getRange().replace(" ", "").replace(",", "").replace("$", "").replace("+", "")));
+                    stotalHigh = stotalHigh.add(new BigDecimal(t.getRange().replace(" ", "").replace(",", "").replace("$", "").replace("+", "")));
                 }
             } else if (t.getTransactionType().equals("Purchase")) {
                 if(t.getRange().contains("-")) {
-                String[] arr = t.getRange().replace(" ", "").replace(",", "").replace("$", "").split("-", 2);
-                ptotalLow += Long.parseLong(arr[0]);
-                ptotalHigh += Long.parseLong(arr[1]);
-                }else{
-                    ptotalLow += Long.parseLong(t.getRange().replace(" ", "").replace(",", "").replace("$", "").replace("+", ""));
-                    ptotalHigh += Long.parseLong(t.getRange().replace(" ", "").replace(",", "").replace("$", "").replace("+", ""));
-
+                    String[] arr = t.getRange().replace(" ", "").replace(",", "").replace("$", "").split("-", 2);
+                    ptotalLow = ptotalLow.add(new BigDecimal(arr[0]));
+                    ptotalHigh = ptotalHigh.add(new BigDecimal(arr[1]));
+                } else {
+                    ptotalLow = ptotalLow.add(new BigDecimal(t.getRange().replace(" ", "").replace(",", "").replace("$", "").replace("+", "")));
+                    ptotalHigh = ptotalHigh.add(new BigDecimal(t.getRange().replace(" ", "").replace(",", "").replace("$", "").replace("+", "")));
                 }
             }
         }
-        output[0] = stotalLow;
-        output[1] = stotalHigh;
-        output[2] = ptotalLow;
-        output[3] = ptotalHigh;
-
+        BigDecimal[] output = new BigDecimal[] { stotalLow, stotalHigh, ptotalLow, ptotalHigh };
         return output;
     }
+
 
 }
